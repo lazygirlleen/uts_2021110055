ae73aa034ac32b6935cb2a3518af74bb8cf827fd
Sulis <sulistiani3399@gmail.com>
2024-10-17 14:12:14 +0700

    Adding Login Screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -8,28 +8,76 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
-      body: Container(
-        color: const Color.fromARGB(255, 224, 242, 228), // Background warna hijau muda
-        child: const Center(
+      appBar: AppBar(
+        flexibleSpace: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
+            Image.asset(
+              'images/Nekoshop.png',
+              fit: BoxFit.contain,
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            
             children: [
-              
-              Text(
-                'Welcome to Vegetables Order App',
+              const Text(
+                'Welcome to Nekoshop',
                 style: TextStyle(
-                  fontSize: 24,
                   fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 6, 116, 13),
+                  fontSize: 20,
                 ),
               ),
-              SizedBox(height: 20), // Spacer for better layout
-             
+              const SizedBox(height: 18),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Display email and password in console
+                    ('Email: ${_emailController.text}');
+                    ('Password: ${_passwordController.text}');
+                  }
+                },
+                child: const Text('Login'),
+              ),
             ],
           ),
         ),
